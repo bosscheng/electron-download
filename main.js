b6e6033f4ab4a60eb48c5efe9fc35546ffcde3d2
bosscheng1210 <25022549@qq.com>
2019-06-13 16:07:04 +0800

    add files

--- main.js
+++ main.js
@@ -13,20 +13,20 @@ function createWindow() {
         webPreferences: {
             nodeIntegration: true
         }
-    })
+    });
 
     // and load the index.html of the app.
-    mainWindow.loadFile('index.html')
+    mainWindow.loadFile('index.html');
 
     // Open the DevTools.
-    mainWindow.webContents.openDevTools()
+    mainWindow.webContents.openDevTools();
 
     // Emitted when the window is closed.
     mainWindow.on('closed', function () {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
-        mainWindow = null
+        mainWindow = null;
     });
 
     ipc.on('showOpenDialog', function (event, arg) {
@@ -41,7 +41,7 @@ function createWindow() {
 
     ipc.on('showErrorBox', function (event, arg) {
         dialog.showErrorBox('提示', arg);
-    })
+    });
 
 }
 
@@ -54,14 +54,14 @@ app.on('ready', createWindow);
 app.on('window-all-closed', function () {
     // On macOS it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') app.quit()
-})
+    if (process.platform !== 'darwin') app.quit();
+});
 
 app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) createWindow()
-})
+    if (mainWindow === null) createWindow();
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
